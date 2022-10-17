155a51be2a37f8370112f1b842838390a5aba589
123ZOKzok <user email>
2022-10-17 10:58:46 +0300

    Done

---
+++ app.rb
@@ -0,0 +1,18 @@
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+# puts [1, 2, 3]
+# This is the equivalent of calling:
+# puts [1, 2, 3].inspect
+# p [1, 2, 3]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# it 'outputs the string "Pass this test, please." using the print method' do
+#     expect { load 'app.rb' }.to output(a_string_including("Pass this test, please.")).to_stdout
+#   end
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
